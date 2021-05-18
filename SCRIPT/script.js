669e08ea53b55d9a94649f8a9f85cb2be055b475
Francisco Sabino <fdasabino@gmail.com>
2021-05-18 07:52:04 +0200

    small changes

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,35 +1,35 @@
 const $lis = document.querySelectorAll("ul li");
 
 $lis.forEach((node) => {
-  node.addEventListener("mousedown", function (event) {
-    const value = node.innerText.trim();
-    const $result = document.querySelector(".result");
-    const resultText = $result.innerText.trim();
+	node.addEventListener("mousedown", function (event) {
+		const value = node.innerText.trim();
+		const $result = document.querySelector(".result");
+		const resultText = $result.innerText.trim();
 
-    if (resultText == "0") {
-      $result.innerText = "";
-    }
-    if (resultText == "undefined") {
-      $result.innerText = "";
-    }
-    if (resultText == "infinity") {
-      $result.innerText = "";
-    }
+		if (resultText == "0") {
+			$result.innerText = "";
+		}
+		if (resultText == "undefined") {
+			$result.innerText = "";
+		}
+		if (resultText == "infinity") {
+			$result.innerText = "";
+		}
 
-    if (resultText == "c") {
-      $result.innerText = "";
-    }
+		if (resultText == "c") {
+			$result.innerText = "";
+		}
 
-    if (value == "=") {
-      let solution = eval(resultText);
-      $result.innerText = solution;
-      return true;
-    }
+		if (value == "=") {
+			let solution = eval(resultText);
+			$result.innerText = solution;
+			return true;
+		}
 
-    if (value.toLowerCase() == "c") {
-      $result.innerText = 0;
-      return true;
-    }
-    $result.append(value);
-  });
+		if (value.toLowerCase() == "c") {
+			$result.innerText = 0;
+			return true;
+		}
+		$result.append(value);
+	});
 });
